e109566bcaf5f3429a2eda1c93e12facc48d17bc
Henrique Gudaites <52537422+Gudaites@users.noreply.github.com>
2020-03-14 19:34:54 -0300

    Migration e model de tarefas

--- src/database/index.js
+++ src/database/index.js
@@ -1,9 +1,11 @@
 import Sequelize from 'sequelize';
+
 import databaseConfig from '../config/database';
 
 import User from '../app/models/User';
+import Task from '../app/models/Task';
 
-const models = [User];
+const models = [User, Task];
 
 class Database {
   constructor() {
@@ -14,7 +16,9 @@ class Database {
     // Conexao banco de dados com models
     this.connection = new Sequelize(databaseConfig);
 
-    models.map(model => model.init(this.connection));
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
   }
 }
 
