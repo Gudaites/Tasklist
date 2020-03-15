6775f0fb8c941b6422ac258e94f37c33b180ee9b
Henrique Gudaites <52537422+Gudaites@users.noreply.github.com>
2020-03-14 21:08:55 -0300

    Definir como feito e deletar tarefas

--- src/app/controllers/TaskController.js
+++ src/app/controllers/TaskController.js
@@ -26,6 +26,40 @@ class TaskController {
     });
     return res.json(tasks);
   }
+
+  async update(req, res) {
+    const { task_id } = req.params;
+
+    const task = await Task.findByPk(task_id);
+
+    // Verifica se tarefa existe
+    if (!task) {
+      return res.status(400).json({ error: 'Task does not exist' });
+    }
+
+    await task.update(req.body);
+
+    return res.json(task);
+  }
+
+  async delete(req, res) {
+    const { task_id } = req.params;
+
+    const task = await Task.findByPk(task_id);
+
+    // Verifica se tarefa existe
+    if (!task) {
+      return res.status(400).json({ error: 'Task does not exist' });
+    }
+
+    if (task.user_id !== req.userId) {
+      return res.status(401).json({ error: 'Unauthorized requestt' });
+    }
+
+    await task.destroy();
+
+    return res.send();
+  }
 }
 
 export default new TaskController();
